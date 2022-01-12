5ae0a8bfff81a95229d8889d1e287f07a4e30710
mhoobler <hoobler.michael@gmail.com>
2022-01-11 23:17:00 -0500

    PORT

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,12 @@ import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../../client/build")));
 }
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("app.listening");
 });
