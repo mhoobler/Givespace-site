caf4acfb61cf17efa21eea72a561ac4ddb8f57ef
mhoobler <hoobler.michael@gmail.com>
2022-01-15 18:01:46 -0500

    cleanup and pass test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Home from "./pages/Home/Home";
-import Catalogue from "./pages/Catalogue/Catalogue";
 import { useQuery } from "@apollo/client";
 import { GET_JWT } from "./graphql/schemas";
+import {Home, Catalogue, CatalogueSelect} from "./pages";
 
 const App = () => {
+  //@ts-ignore
   const jwt = useQuery(GET_JWT);
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/lists/:user_id' element={<Home />} />
+        <Route path='/lists/:user_id' element={<CatalogueSelect />} />
         <Route path='/list/:catalogue_id' element={<Catalogue />} />
       </Routes>
     </Router>
