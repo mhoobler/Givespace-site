d9ab5d2312fc3b55555ab2b8736109f3fc850a8a
mhoobler <hoobler.michael@gmail.com>
2022-01-16 15:45:10 -0500

    added routing, some types, and webpack config tweaks

--- client/src/pages/CatalogueSelect/CatalogueSelect.tsx
+++ client/src/pages/CatalogueSelect/CatalogueSelect.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+const CatalogueRow: React.FC<CatalogueListItem> = ({
+  id,
+  //user_id,
+  title,
+  //created,
+  //updated,
+}) => {
+  return (
+    <div className="row">
+      <div className="col-8">{title}</div>
+      <div className="col-4">
+        <Link className="btn btn-primary" to={`/list/${id}`}>
+          Go
+        </Link>
+        <button className="btn btn-danger">Del</button>
+      </div>
+    </div>
+  );
+};
 
 const CatalogueSelect = () => {
+  //@ts-ignore
+  const { user_id } = useParams();
+
+  // Some kind of Query;
+  const results: CatalogueList = [
+    {
+      id: "id1",
+      user_id: "id",
+      title: "title1",
+      created: new Date().toString(),
+      updated: new Date().toString(),
+    },
+    {
+      id: "id2",
+      user_id: "id",
+      title: "title2",
+      created: new Date().toString(),
+      updated: new Date().toString(),
+    },
+  ];
+
   return (
     <div>
-      CatalogueSelect
+      <div className="row">
+        <div className="col-2">
+          <Link className="btn btn-primary" to={`/`}>
+            Go Back
+          </Link>
+        </div>
+      </div>
+      {results.map((e: CatalogueListItem) => (
+        <CatalogueRow key={e.id} {...e} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CatalogueSelect
+export default CatalogueSelect;
--- client/webpack.common.js
+++ client/webpack.common.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
