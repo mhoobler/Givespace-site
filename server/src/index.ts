2f06217fe873491d01e48f0c3fc0052e6d191255
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-01-15 23:05:23 -0500

    dotenv fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
 import { app, httpServer } from "./app";
 import dotenv from "dotenv";
 import apolloServer from "./graphql";
-
+import { Request, Response } from "express";
 dotenv.config();
+import db from "../db";
 
 const startServers = async () => {
+  app.get("/test", async (req: Request, res: Response) => {
+    try {
+      const { rows } = await db.query("SELECT * FROM catalogues");
+      console.log(rows);
+      res.send({ message: rows });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
+
   httpServer.listen(process.env.PORT, () =>
     console.log(
       `🚀 Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
