b1ebccb4d69493c8e6d75b93b2524c6141123dfa
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-01-17 18:21:58 -0500

    create catalogue & home page

--- client/src/pages/Catalogue/Catalogue.tsx
+++ client/src/pages/Catalogue/Catalogue.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-// import { JUNK_QUERY } from "../../graphql/schemas";
 
 const Catalogue = () => {
   const { catalogue_id } = useParams();
@@ -9,7 +7,6 @@ const Catalogue = () => {
 
   // @ts-ignore
   // Some kind of Query or Subscription
-  // let catalogueData = useQuery(JUNK_QUERY, { variables: { id: catalogue_id } });
 
   const goBack = () => navigate(-1);
 
@@ -24,9 +21,6 @@ const Catalogue = () => {
       </div>
       <div className="row">
         <div className="col-12">Catalogue: {catalogue_id}</div>
-        {/* {catalogueData.data && (
-          <div className="col-12">User: {catalogueData.data.user_id}</div>
-        )} */}
       </div>
     </div>
   );
