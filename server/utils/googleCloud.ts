373aeb8094a8ed44589b9240a768d29341862ac0
mhoobler <hoobler.michael@gmail.com>
2022-02-18 21:49:05 -0500

    replace line breaks

--- server/utils/googleCloud.ts
+++ server/utils/googleCloud.ts
@@ -8,7 +8,7 @@ const storage = new Storage({
     type: process.env.GC_TYPE,
     project_id: process.env.GC_PROJECT_ID,
     private_key_id: process.env.GC_PRIVATE_KEY_ID,
-    private_key: process.env.GC_PRIVATE_KEY,
+    private_key: process.env.GC_PRIVATE_KEY.replace(/\\n/gm, '\n'),
     client_email: process.env.GC_CLIENT_EMAIL,
     client_id: process.env.GC_CLIENT_ID,
     auth_uri: process.env.GC_AUTH_URI,
